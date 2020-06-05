8f29e2945a789bd39b56e005c47d4c71566eb0c9
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-05 11:04:40 -0700

    add third jquery input, output

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,17 @@ $(document).ready(function() {
 
     let pizza = $("input[name='pizza']:checked").val();
     let band = $("input[name='band']:checked").val();
+    let time = $("input[name='time']:checked").val();
+
       console.log(pizza);
       console.log(band);
+      console.log(time);
 
-    if (pizza === 'cheese' && band === 'mcgraw') {
+    if (pizza === 'cheese' && band === 'mcgraw' || time === 'couch') {
       $('#pizzaCheese').show();
-    } else if (pizza === 'pep' && band === 'bon') {
+    } else if (pizza === 'pep' && band === 'bon' || time === 'jog' ) {
       $('#pizzaPep').show();
-    } else if (pizza === 'mush' && band === 'kiwanuka"') {
+    } else if (pizza === 'mush' && band === 'kiwanuka' || time === 'pizza') {
       $('#pizzaMush').show();
     }
       
